3c09d5916f76ab0639475b1b8d58794e4acb053e
Jens Wille <jens.wille@hbz-nrw.de>
2026-05-21 12:02:36 +0200

    Disable XML entity limits for all configurations, not just Alma. (f4c7461, 82e2f4a)

--- main/src/main/java/hbz/limetrans/Limetrans.java
+++ main/src/main/java/hbz/limetrans/Limetrans.java
@@ -209,6 +209,13 @@ public class Limetrans { // checkstyle-disable-line ClassDataAbstractionCoupling
 
         mType = aType;
 
+        // Export ("PubHub") BGZF contains a large XML file, so does Baybib (OAI-PMH); increase limits for XML parser.
+        // »The accumulated size of entities is "50,000,001" that exceeded the "50,000,000" limit set by "FEATURE_SECURE_PROCESSING".«
+        // »JAXP00010003: The length of entity "[xml]" is "100,001" that exceeds the "100,000" limit set by "jdk.xml.maxGeneralEntitySizeLimit".«
+        // https://docs.oracle.com/en/java/javase/25/security/java-api-xml-processing-jaxp-security-guide.html#GUID-82F8C206-F2DF-4204-9544-F96155B1D258__TABLE_YH5_5R4_1VB
+        System.setProperty("jdk.xml.totalEntitySizeLimit", "0");
+        System.setProperty("jdk.xml.maxGeneralEntitySizeLimit", "0");
+
         initializeInput(aSettings);
         initializeVars(aSettings);
 
@@ -304,13 +311,6 @@ public class Limetrans { // checkstyle-disable-line ClassDataAbstractionCoupling
     }
 
     private String initializeAlma(final Settings aSettings) { // checkstyle-disable-line JavaNCSS
-        // Export ("PubHub") BGZF contains a large XML file; increase limits for XML parser.
-        // »The accumulated size of entities is "50,000,001" that exceeded the "50,000,000" limit set by "FEATURE_SECURE_PROCESSING".«
-        // »JAXP00010003: The length of entity "[xml]" is "100,001" that exceeds the "100,000" limit set by "jdk.xml.maxGeneralEntitySizeLimit".«
-        // https://docs.oracle.com/en/java/javase/25/security/java-api-xml-processing-jaxp-security-guide.html#GUID-82F8C206-F2DF-4204-9544-F96155B1D258__TABLE_YH5_5R4_1VB
-        System.setProperty("jdk.xml.totalEntitySizeLimit", "0");
-        System.setProperty("jdk.xml.maxGeneralEntitySizeLimit", "0");
-
         final Settings almaSettings = aSettings.getAsSettings("alma");
 
         // Ex Libris (Deutschland) GmbH
